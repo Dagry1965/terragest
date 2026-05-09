e3df5b43682d2ae2d8858da3a4156541cecba001
Dagry Herve <ehivet@yahoo.fr>
2026-05-09 08:28:25 +0200

    Stabilize ERP dashboard advanced exports

--- src/components/erp/dashboard/index.ts
+++ src/components/erp/dashboard/index.ts
@@ -1,3 +1,5 @@
 export * from "./ERPDashboardMetrics";
 export * from "./ERPDashboardPanel";
 export * from "./ERPDashboardSection";
+export * from "./ERPDashboardActivityFeed";
+export * from "./ERPDashboardQuickActions";
