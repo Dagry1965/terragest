f47e00b03be5c7e2aa1b9111fbb8ee6386616b04
Dagry Herve <ehivet@yahoo.fr>
2026-05-09 08:32:13 +0200

    Stabilize ERP runtime cockpit exports

--- src/components/erp/runtime/index.ts
+++ src/components/erp/runtime/index.ts
@@ -3,3 +3,6 @@ export { ERPRuntimeTable } from "./ERPRuntimeTable";
 export { ERPRuntimeForm } from "./ERPRuntimeForm";
 export { ERPRuntimeDetails } from "./ERPRuntimeDetails";
 export { ERPRuntimeFieldValue } from "./ERPRuntimeFieldValue";
+
+export * from "./ERPRuntimeStatusPanel";
+export * from "./ERPRuntimeAlertsPanel";
